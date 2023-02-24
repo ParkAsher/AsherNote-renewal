2b81794681d8b97f9868be0d06abbb201f39f843
ParkAsher <devasherpark@naver.com>
2023-02-24 23:26:49 +0900

    회원가입 로직

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/user')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post('/register')
+    async createUser(@Body() body: CreateUserDto) {
+        await this.userService.createUser(body);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Users } from 'src/entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(Users) private usersRepository: Repository<Users>,
+    ) {}
+
+    async createUser(body: CreateUserDto) {
+        const user = await this.usersRepository.findOne({
+            where: { email: body.email },
+        });
+
+        if (user) {
+            throw new UnauthorizedException('이미 존재하는 사용자입니다.');
+        }
+
+        const hashedPassword = await bcrypt.hash(body.password, 10);
+
+        await this.usersRepository.save({
+            email: body.email,
+            nickname: body.nickname,
+            password: hashedPassword,
+        });
+    }
+}
